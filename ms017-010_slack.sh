2eb9f41f0820580552ad08d5791ea31b8bd32e09
jeredbare <jeredbare@gmail.com>
2017-05-15 10:55:40 -0500

    Update ms017-010_slack.sh

--- ms017-010_slack.sh
+++ ms017-010_slack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nmap --script smb-vuln-ms17-010.nse -p445 $0 >> ms17-010_hosts.txt
-curl -F file=@"ms17-010_hosts.txt" -F initial_comment="MS17-010 Vulerable Machines" -F channels=#general -F token=[insert api token here] https://slack.com/api/files.upload >> /dev/null
+curl -F file=@"ms17-010_hosts.txt" -F initial_comment="MS17-010 Vulerable Machines" -F channels=[insert channel(s) here] -F token=[insert api token here] https://slack.com/api/files.upload >> /dev/null
